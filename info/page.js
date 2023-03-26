9fc0f008785c01bb9866f38a7be545165cd3c679
RSKCS2 <rskcs2.com@gmail.com>
2023-03-26 09:57:35 +0200

    RSKCS2.COM

---
+++ info/page.js
@@ -0,0 +1,5 @@
+// coded by @Fvrqan | Furqan Maudhudy
+// inspired by Kevin's Pen | https://codepen.io/Kass09/pen/NBpdrP
+// Saturday, 29 December 2018
+const projectName = 'technical-documentation-page';
+localStorage.setItem('freeCodeCamp_project', 'Technical Documentation Page');
